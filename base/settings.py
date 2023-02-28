5562b7ed4687b8221f0e6e4746be55823c791450
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-03-01 01:05:09 +0300

    django-search engine

--- base/settings.py
+++ base/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = "base.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-
+ """
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 

2e39cd6bf05c2c80148f19346419c906f4db10b6
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-03-01 07:52:07 +0300

    django-search engine

--- base/settings.py
+++ base/settings.py
@@ -21,7 +21,6 @@ API_KEY = os.environ.get('API_KEY')
 SEARCH_ENGINE_ID= os.environ.get('SEARCH_ENGINE_ID')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
